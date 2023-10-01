eb8161ffbd9bc91c8103f436b835d8476e7e8efa
imxasik <imxasik@gmail.com>
2023-10-01 11:29:52 +0600

    xp web

---
+++ wx/index.php
@@ -0,0 +1,10 @@
+<?php
+$target_dir = 'images/';
+$target_file = $target_dir . basename($_FILES["image"]["name"]);
+
+if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+    echo 'Image successfully uploaded to the web server.';
+} else {
+    echo 'Failed to upload image to the web server.';
+}
+?>
